a22d3d76e4e049b144082012d9107309bb1c0eaa
tdoric <tdoric8@gmail.com>
2016-12-19 23:20:12 +0100

    Dodavanje Internet permissiona i firebase auth u gradle od app za potrebe autentifikacije kreiranje sign up (trenutno ne radi)

--- BeerMate/app/src/main/java/hr/foi/air/beermate/MainActivity.java
+++ BeerMate/app/src/main/java/hr/foi/air/beermate/MainActivity.java
@@ -4,13 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ListView;
-import android.widget.TextView;
-
-import com.firebase.ui.database.FirebaseListAdapter;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openBeers(View view){
-        Intent intent = new Intent(this,Beers.class);
+        Intent intent = new Intent(this,BeersListActivity.class);
         startActivity(intent);
     }
 }
