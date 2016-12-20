890a4e2cf8360470052b8277567ffbbfec91ec51
tdoric <tdoric8@gmail.com>
2016-12-20 15:48:58 +0100

    Dodana aktivnost za registraciju, preimenovana aktivnost za login, otkrivena greska, genymotion ne podrzava google authentication. testiranje na mobitelu

--- BeerMate/app/src/main/java/hr/foi/air/beermate/LoginActivity.java
+++ BeerMate/app/src/main/java/hr/foi/air/beermate/LoginActivity.java
@@ -1,6 +1,7 @@
 package hr.foi.air.beermate;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -15,13 +17,14 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     //defining view objects
     private EditText editTextEmail;
     private EditText editTextPassword;
     private Button buttonSignup;
     private ProgressDialog progressDialog;
+    private TextView  textViewRegister;
 
 
     //defining firebaseauth object
@@ -30,7 +33,7 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_login);
 
         //initializing firebase auth object
         firebaseAuth = FirebaseAuth.getInstance();
@@ -38,16 +41,17 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         //initializing views
         editTextEmail = (EditText) findViewById(R.id.editTextEmail);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
-
+        textViewRegister = (TextView) findViewById(R.id.textViewRegister);
         buttonSignup = (Button) findViewById(R.id.buttonSignup);
 
         progressDialog = new ProgressDialog(this);
 
         //attaching listener to button
         buttonSignup.setOnClickListener(this);
+        textViewRegister.setOnClickListener(this);
     }
 
-    private void registerUser(){
+    private void loginUser(){
 
         //getting email and password from edit texts
         String email = editTextEmail.getText().toString().trim();
@@ -67,31 +71,44 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         //if the email and password are not empty
         //displaying a progress dialog
 
-        progressDialog.setMessage("Registering Please Wait...");
+        progressDialog.setMessage("Login Please Wait...");
         progressDialog.show();
 
-        //creating a new user
-        firebaseAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        //checking if success
-                        if(task.isSuccessful()){
-                            //display some message here
-                            Toast.makeText(SignUpActivity.this,"Successfully registered",Toast.LENGTH_LONG).show();
-                        }else{
-                            //display some message here
-                            Toast.makeText(SignUpActivity.this,"Registration Error",Toast.LENGTH_LONG).show();
-                        }
-                        progressDialog.dismiss();
-                    }
-                });
+        //activity_login in app
+       firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+           @Override
+           public void onComplete(@NonNull Task<AuthResult> task) {
+              if(task.isSuccessful()){
+                  progressDialog.dismiss();
+                  Toast.makeText(LoginActivity.this,"Login successfully ",Toast.LENGTH_SHORT).show();
+                  Intent i = new Intent(LoginActivity.this,MainActivity.class);
+                  startActivity(i);
+              }
+               else{
+                  progressDialog.dismiss();
+                  Toast.makeText(LoginActivity.this,"Error with activity_login!",Toast.LENGTH_SHORT).show();
+              }
+           }
+       });
 
     }
 
     @Override
     public void onClick(View view) {
-        //calling register method on click
-        registerUser();
+
+        switch (view.getId()){
+            case R.id.buttonSignup:
+                  loginUser();
+                break;
+            case R.id.textViewRegister:
+                openRegister();
+                break;
+
+        }
+    }
+
+    private void openRegister() {
+        Intent i = new Intent(LoginActivity.this,RegisterActivity.class);
+        startActivity(i);
     }
 }
---
+++ BeerMate/app/src/main/java/hr/foi/air/beermate/RegisterActivity.java
@@ -0,0 +1,84 @@
+package hr.foi.air.beermate;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnRegister,btnBack;
+    EditText editTextUsername,editTextMail,editTextPasswordRegister,editTextRepeatPassword;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        editTextUsername=(EditText)findViewById(R.id.editTextUsername);
+        editTextMail=(EditText)findViewById(R.id.editTextMailRegister);
+        editTextPasswordRegister=(EditText)findViewById(R.id.editTextPasswordRegister);
+        editTextRepeatPassword=(EditText)findViewById(R.id.editTextRepeatPassword);
+        btnRegister= (Button) findViewById(R.id.buttonRegister);
+        btnBack = (Button) findViewById(R.id.buttonBack);
+
+        btnRegister.setOnClickListener(this);
+        btnBack.setOnClickListener(this);
+        progressDialog = new ProgressDialog(this);
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    private void registerUser(){
+
+        String mail = editTextMail.getText().toString();
+        String password  = editTextPasswordRegister.getText().toString();
+
+        progressDialog.setMessage("Registering Please Wait...");
+        progressDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(mail,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    progressDialog.dismiss();
+                    Toast.makeText(RegisterActivity.this,"Registering successfully ",Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(RegisterActivity.this,LoginActivity.class);
+                    startActivity(i);
+                }
+                else{
+                    progressDialog.dismiss();
+                    Toast.makeText(RegisterActivity.this,"Error with registering!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.buttonRegister:
+                registerUser();
+                break;
+            case R.id.buttonBack:
+                backToLogin();
+                break;
+        }
+    }
+
+    private void backToLogin() {
+        Intent i = new Intent(RegisterActivity.this,LoginActivity.class);
+        startActivity(i);
+    }
+}
