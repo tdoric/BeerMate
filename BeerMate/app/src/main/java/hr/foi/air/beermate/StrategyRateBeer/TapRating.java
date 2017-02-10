ac3730880775b3689daf5bc6b6e4efa57fb4e077
tdoric <tdoric8@gmail.com>
2017-02-10 20:13:28 +0100

    Dodavanje druge strategije kako bi se implementirala modularnost

---
+++ BeerMate/app/src/main/java/hr/foi/air/beermate/StrategyRateBeer/TapRating.java
@@ -0,0 +1,81 @@
+package hr.foi.air.beermate.StrategyRateBeer;
+
+import android.app.Dialog;
+import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import hr.foi.air.beermate.R;
+import hr.foi.air.beermate.RatingUtil;
+
+/**
+ * Created by Tomislav on 2/8/2017.
+ */
+
+public class TapRating extends RecyclerView.ViewHolder implements StrategyBeer {
+    View mView;
+    Dialog rankDialog;
+    int vote;
+    RatingUtil ratingUtil = new RatingUtil();
+
+    public TapRating(View itemView) {
+        super(itemView);
+        mView=itemView;
+    }
+
+
+    @Override
+    public void rateBeer( final String id, String name, float averageRate, final int totalVotes, final int numberOfVotes) {
+
+        vote = Math.round(averageRate);
+
+        rankDialog = new Dialog(mView.getContext(), R.style.FullHeightDialog);
+        rankDialog.setContentView(R.layout.tap_rank_dialog);
+        rankDialog.setCancelable(true);
+
+        TextView text = (TextView) rankDialog.findViewById(R.id.tap_rank_dialog_text);
+        text.setText(name);
+
+        final TextView textRate = (TextView) rankDialog.findViewById(R.id.tap_rank_dialog_textRate);
+        textRate.setText(Integer.toString(vote));
+        textRate.setTextColor(Color.GREEN);
+
+        textRate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (vote < 5) {
+                    vote++;
+                    textRate.setTextColor(Color.GREEN);
+                } else {
+                    vote = 0;
+                    textRate.setTextColor(Color.RED);
+                }
+                textRate.setText(Integer.toString(vote));
+            }
+        });
+
+        Button rateBtn = (Button) rankDialog.findViewById(R.id.tap_rank_dialog_rateBtn);
+        rateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float newRating = ratingUtil.calculateNewRating(vote, totalVotes, numberOfVotes);
+                ratingUtil.updateVotes(id, totalVotes+vote, numberOfVotes+1);
+                rankDialog.dismiss();
+                Toast.makeText(mView.getContext(), Float.toString(newRating), Toast.LENGTH_LONG).show(); //TODO - obrisati
+                rankDialog.dismiss();
+            }
+        });
+        Button cancelBtn = (Button) rankDialog.findViewById(R.id.tap_rank_dialog_cancelBtn);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rankDialog.dismiss();
+            }
+        });
+        rankDialog.show();
+
+    }
+}
